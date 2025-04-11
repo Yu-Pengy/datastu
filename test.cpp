72c4f773b77b24cf37271e9840dad59e6f588400
Yu-Yan-Peng <17702437152@163.com>
2025-04-11 22:11:12 +0800

    first save

---
+++ test.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int a = 1;
+	int b = 2;
+	cout << "hello world!" << endl;
+	cout << "I love code" << endl;
+	cout << "I love you" << endl;
+	return 0;
+}
---
+++ try.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+	cout << "hello world!" << endl;
+	cout << "I love code" << endl;
+	//cout << "I love you" << endl;
+	return 0;
+}
