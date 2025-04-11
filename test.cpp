2597a8c65fe171f1f662e0f7bc22d2bd0fb59d55
Yu-Yan-Peng <17702437152@163.com>
2025-04-11 22:30:14 +0800

    third change

--- test.cpp
+++ test.cpp
@@ -5,6 +5,7 @@ int main(){
 	int b = 2;
 	cout << "hello world!" << endl;
 	cout << "I love code" << endl;
+	cout << "I love learn new things" << endl;
 	cout << "I love you" << endl;
 	cout << "I like it" << endl;
 	return 0;
